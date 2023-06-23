ed79726431cc8b02f358b0688d5f9a6013613cac
Matt Newsted <mnewsted@illinoislegalaid.org>
2023-06-23 12:17:20 -0500

    EconomicRemediesMiscRemedies

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.OrderOfProtection',
-      version='0.0.30',
+      version='0.0.31',
       description=('A docassemble extension.'),
       long_description='# docassemble.OrderOfProtection\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
