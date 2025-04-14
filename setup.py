35ac392b77e236113d3395ff7d4a910b5ccc0966
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-04-14 14:29:44 -0500

    interview improvements to align style and respond to feedback

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from fnmatch import fnmatchcase
 from distutils.util import convert_path
 
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.OrderOfProtection',
-      version='1.0.15',
+      version='1.0.16',
       description=('A docassemble extension.'),
       long_description='# docassemble.OrderOfProtection\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
@@ -52,8 +52,7 @@ setup(name='docassemble.OrderOfProtection',
       author_email='mnewsted@illinoislegalaid.org',
       license='The MIT License (MIT)',
       url='https://docassemble.org',
-      packages=find_packages(),
-      namespace_packages=['docassemble'],
+      packages=find_namespace_packages(),
       install_requires=[],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/OrderOfProtection/', package='docassemble.OrderOfProtection'),
