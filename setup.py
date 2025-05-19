d5450be9afc6585650996f6768d20a3d3c332960
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-05-19 17:00:42 -0500

    alignment with cook eop adjustments

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.OrderOfProtection',
-      version='1.0.47',
+      version='1.0.48',
       description=('A docassemble extension.'),
       long_description='# docassemble.OrderOfProtection\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
