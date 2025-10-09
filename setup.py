71eef999b52d2258e0ce24c3fb3c816bc9cb81bc
Matt Newsted <mnewsted@illinoislegalaid.org>
2025-10-09 10:26:36 -0500

    updated program after more testing

--- setup.py
+++ setup.py
@@ -44,13 +44,13 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.OrderOfProtection',
-      version='1.1.7',
+      version='1.1.8',
       description=('A docassemble extension.'),
       long_description='# docassemble.OrderOfProtection\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nMatt Newsted, mnewsted@illinoislegalaid.org\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Matt Newsted',
       author_email='mnewsted@illinoislegalaid.org',
-      license='MIT',
+      license='',
       url='https://docassemble.org',
       packages=find_namespace_packages(),
       install_requires=[],
